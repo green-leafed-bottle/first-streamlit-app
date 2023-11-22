a7793120c2ee381040a72fe5e315607f4c1bd49e
green-leafed-bottle <151791663+green-leafed-bottle@users.noreply.github.com>
2023-11-22 23:15:48 +0100

    add header & some texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title("Nothing to see here")
+streamlit.title("Building a restaurant, aren't we?")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
