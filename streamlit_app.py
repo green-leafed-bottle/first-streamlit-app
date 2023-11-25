5bc5fd33b867dc9de796da74b84b7147a8a9dc26
green-leafed-bottle <151791663+green-leafed-bottle@users.noreply.github.com>
2023-11-25 21:26:19 +0100

    button to fetch fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,14 +40,17 @@ try:
 except URLError:
     streamlit.error()
 
-streamlit.stop()
+def get_fruit_load_list(conn):
+    with conn.cursor() as cur:
+        cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+if streamlit.button("Get fruit load list"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    streamlit.header("The fruit load list contains:")
+    streamlit.dataframe(get_fruit_load_list(my_cnx))
+
+streamlit.stop()
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfruit')
