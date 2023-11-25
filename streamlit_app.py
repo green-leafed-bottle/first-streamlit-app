70a2d10f111c478fac65adc6abd537e6ba4a0ef2
green-leafed-bottle <151791663+green-leafed-bottle@users.noreply.github.com>
2023-11-25 21:31:50 +0100

    move insert into button-launched action

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,10 +50,13 @@ if streamlit.button("Get fruit load list"):
     streamlit.header("The fruit load list contains:")
     streamlit.dataframe(get_fruit_load_list(my_cnx))
 
-streamlit.stop()
+def insert_row_snowflake(conn, fruit):
+    with conn.cursor() as cur:
+        cur.execute(f"insert into fruit_load_list values ('{fruit}')")
+        return "Thanks for adding " + fruit
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add')
+if streamlit.button("Add to the fruit list"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    streamlit.text(insert_row_snowflake(my_cnx, add_my_fruit)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
